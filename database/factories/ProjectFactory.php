62a6613d2335b96cfb0006c32c6241bc65135799
Paolo Paz Ramos <paolo@idw.com.pe>
2021-12-12 16:24:12 -0500

    migracion, modelos y seeders

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use App\Models\User;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Project::class;
+
+
+    public function definition()
+    {
+        return [
+            "name" => $this->faker->company,
+            "description" => $this->faker->paragraph(3),
+            "user_id" => User::all()->random()->id,
+            "created_at" => now()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::factory()->times(1)->create([
+            "name" => "cursosdesarrolloweb",
+            "email" => "app@cursosdesarrolloweb.es",
+            "password" => bcrypt("password")
+        ]);
+        Project::factory()->times(40)->create();
+
     }
 }
