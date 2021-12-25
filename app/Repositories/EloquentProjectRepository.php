14903d2d11c205d7fd2a4c6d8077164279168bb1
Paolo Paz Ramos <paolo@idw.com.pe>
2021-12-25 17:27:16 -0500

    implementar logica para las operaciones CRUD desde repositorio

--- app/Repositories/EloquentProjectRepository.php
+++ app/Repositories/EloquentProjectRepository.php
@@ -6,28 +6,36 @@ use App\Models\Project;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class EloquentProjectRepository implements ProjectRepositoryInterface {
+
     public function paginated()
     {
-        //
+        return ProjectResource::collection(Project::with(["user"])
+            ->orderByDesc("id")
+            ->paginate(5)
+        );
     }
 
     public function create()
     {
-        //
+        return Project::create(request()->only("name", "description"));
     }
 
     public function update(int $id)
     {
-        //
+        return Project::where('id', $id)
+            ->update(request()->only("name", "description"));
     }
 
     public function delete(int $id)
     {
-        //
+        return Project::destroy($id);
     }
 
     public function find(int $id)
     {
-        //
+        if (null == $project = Project::find($id)) {
+            throw new ModelNotFoundException("Proyecto no encontrado");
+        }
+        return new ProjectResource($project);
     }
 }
