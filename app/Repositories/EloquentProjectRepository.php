56d124e9ba4f346a897348172bb321fc1b039f49
Paolo Paz Ramos <paolo@idw.com.pe>
2021-12-25 17:21:22 -0500

    crear interfaces para aplicar e iniciar clase que implemente la interfaz

---
+++ app/Repositories/EloquentProjectRepository.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Repositories;
+
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class EloquentProjectRepository implements ProjectRepositoryInterface {
+    public function paginated()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function update(int $id)
+    {
+        //
+    }
+
+    public function delete(int $id)
+    {
+        //
+    }
+
+    public function find(int $id)
+    {
+        //
+    }
+}
---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Repositories;
+
+interface RepositoryInterface {
+    public function paginated();
+    public function create();
+    public function update(int $id);
+    public function delete(int $id);
+    public function find(int $id);
+}
